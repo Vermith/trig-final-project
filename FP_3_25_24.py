a1f1587c526cd2517277c9c7fb706ca2a59c8ebc
Adrian Roper <105611655+Vermith@users.noreply.github.com>
2024-03-27 19:38:38 -0400

    Add files via upload
    initial creation of the program, quickly scrapped as it was needlessly complicated.

---
+++ FP_3_25_24.py
@@ -0,0 +1,30 @@
+# Final Project COP1500
+# Trignomtric Function Plotter
+
+import math
+import mathplotlib
+
+# trig function calculator
+
+def trigSolver(a,b,c,d):
+    # variables
+
+   a =
+   b =
+   c =
+   d =
+
+    # print possible functions
+    print("Possible functions include")
+    print("COS,SINE,TAN")
+
+    prompt = input("Enter the Desired Function")
+
+    if prompt == COS:
+            a = int(input("Enter the amplitude")
+            b = int(input("Enter the period"))
+            phaseShift = input("is there phaseshift?")
+            if phaseShift == True:
+                c = int(input("Enter the phaseshift"))
+            else:
+                
